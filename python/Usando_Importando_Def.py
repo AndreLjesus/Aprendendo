49c5e3098c100e813734241ba79a570c953813b8
AndreLjesus <143151297+AndreLjesus@users.noreply.github.com>
2024-03-06 11:25:59 -0300

    Update Usando_Importando_Def.py

--- python/Usando_Importando_Def.py
+++ python/Usando_Importando_Def.py
@@ -1,6 +1,5 @@
 from Funções import *
 
-
 #Teste aprendendo a usar def
 while True:
     nome = input("Digite seu nome: ")
@@ -10,11 +9,9 @@ while True:
         break
 
 
-
 #Curso em Video: Exercicio 96
 base = float(input("Digite a base do quadrado: "))
 area_quadrado(base)
-
 
 
 #Curso em Video: Exercicio 98
@@ -27,5 +24,24 @@ lista = []
 for c in range(0,5):
     a = int(input("Digite um valor: "))
     lista.append(a)
-
 ordenar_lista(lista)
+
+
+
+
+#contador
+contador(2,10,2)
+
+
+#lista
+valores = [5,7,3,1]
+dobra(valores)
+
+
+#Calculadora area Terrenos: Exercico 96
+terreno()
+
+
+#Adaptar texto: Exercicio 97
+lista = input("Digite uma frase: ")
+adaptar(lista)
